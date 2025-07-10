400af434e67eb97ff132973cfd7c541c8f6282d9
LBielskus <liudbiel@gmail.com>
2025-07-10 22:54:07 +0300

    Fix: Clean up .env.local and refactor data fetching

--- middleware.ts
+++ middleware.ts
@@ -10,5 +10,12 @@ export function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/:path*',
+  /*
+   * Match all request paths except for the ones starting with:
+   * - api (API routes)
+   * - _next/static (static files)
+   * - _next/image (image optimization files)
+   * - favicon.ico (favicon file)
+   */
+  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
 };
