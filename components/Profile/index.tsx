4047341c12a4dd70820cf9993d7e1df4857eeabd
LBielskus <liudbiel@gmail.com>
2025-07-10 21:49:37 +0300

    Fix: Remove all references to next-auth to resolve build failure

--- components/Profile/index.tsx
+++ components/Profile/index.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import Button from '../Button';
-import { Container, UserEmail, UserName, Wrapper } from './UserProfileElements';
-import { useSession, signOut } from 'next-auth/react';
+// import React from 'react';
+// import Button from '../Button';
+// import { Container, UserEmail, UserName, Wrapper } from './UserProfileElements';
+// import { useSession, signOut } from 'next-auth/react';
 
-const UserProfile = () => {
-  const { data: session }: any = useSession();
+// const UserProfile = () => {
+//   const { data: session }: any = useSession();
 
-  return (
-    <Container>
-      <Wrapper>
-        {session && (
-          <>
-            <UserName>{`Hello ${session?.user?.fullName}`}</UserName>
-            <UserEmail>{session?.user?.email}</UserEmail>
+//   return (
+//     <Container>
+//       <Wrapper>
+//         {session && (
+//           <>
+//             <UserName>{`Hello ${session?.user?.fullName}`}</UserName>
+//             <UserEmail>{session?.user?.email}</UserEmail>
 
-            <Button title='Logout' onClick={signOut} />
-          </>
-        )}
-      </Wrapper>
-    </Container>
-  );
-};
+//             <Button title='Logout' onClick={signOut} />
+//           </>
+//         )}
+//       </Wrapper>
+//     </Container>
+//   );
+// };
 
-export default UserProfile;
+// export default UserProfile;
